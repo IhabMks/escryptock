25e1f8b443e595c9d96b83f5775301af68ffe7d7
IhabMks <ihab.ozark@gmail.com>
2022-07-07 00:36:08 +0200

    initial commit

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 from datetime import date, datetime
 import yfinance as yf
 import pandas as pd
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 def main():
