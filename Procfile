d5fe0a1416ddc2f92d41011a921342727a6c4a45
IhabMks <ihab.ozark@gmail.com>
2022-07-12 23:32:20 +0200

    removed duplicated lines

--- Procfile
+++ Procfile
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 web: sh setup.sh && streamlit run app.py
 heroku ps:scale web=1
-=======
-heroku ps:scale web=1
-web: sh setup.sh && streamlit run app.py
->>>>>>> 22b5fc93148a5d2a6d625392e998c6383a1809de
--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"email@domain\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
@@ -11,18 +10,4 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-=======
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"email@domain\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
->>>>>>> 22b5fc93148a5d2a6d625392e998c6383a1809de
 " > ~/.streamlit/config.toml
