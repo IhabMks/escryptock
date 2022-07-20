2b7d32aafb25147d2f86fbb6b83467157379c5bd
IhabMks <ihab.ozark@gmail.com>
2022-07-21 00:45:55 +0200

    Added News feed and Trending ticker on home screen

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 import json
-
 PAGE_CONFIG = {"page_title":"StkForecast.io","page_icon":"chart_with_upwards_trend","layout":"wide"}
 st.set_page_config(**PAGE_CONFIG)
 st.markdown("""
     <style>
       @import 'https://fonts.googleapis.com/css?family=Montserrat';
       
+      section[data-testid="stSidebar"] > div {
+      width: 19.5rem;}
       *{
       font-family: 'Montserrat';
       color: Darkblue;
@@ -112,19 +113,9 @@ st.markdown("""
       blockquote:hover p {
         opacity: 0.5;
         transition: opacity 0.2s ease;}
-      home_col_1_2_header{
-        font-weight: normal;
-        letter-spacing: -1px;
-        color: #34495E;}
-      table{
-        border-spacing: initial;
-        border-collapse: collapse;
-        background: #fff;
-        border-radius: 3px;
-        overflow: hidden;
-        width: 100%;
-        margin: 0 auto;
-        position: relative;}
+
+                                              /*Home styling*/
+                                              /*Trending tickers*/
 
       table tr:nth-child(even) td{
       background:#dedede66;}
@@ -132,11 +123,11 @@ st.markdown("""
       table tr:nth-child(odd) td{
         background:#fff;}
       
-      table * {
-        position: relative;}
+      table {
+        width: 100%}
       
       table thead tr {
-        height: 60px;
+        height: 40px;
         background: #36304a;}
       
       .table100-head th {
@@ -145,52 +136,79 @@ st.markdown("""
         border: 0;
         line-height: 1.2;
         font-weight: unset;}
-      
-      .column1 {
-        width: 260px;
-        padding-left: 40px;}
-      .column2 {
-        width: 160px;}
-      .column3 {
-        width: 245px;}
-      .column4 {
-        width: 245px;
-        text-align: right;
-        padding-right: 62px}
-
-      table td, table th {
-        text-align: left;
-        padding-left: 8px;}
-
-      th {
-        text-align: left}
-      
-      table tbody tr {
-        height: 50px;}
-      
-      tbody tr {
-        font-family: Montserrat;
-        font-size: 15px;
-        line-height: 1.2;
-        font-weight: unset;}
-      
+      .column1{
+        white-space: nowrap;}       
       .css-vba4y6 th, .css-vba4y6 td, .css-vba4y6 tr {
         border:0;}
 
+                                                /*News feed*/
+      .single-blog-post.post-style-2 {
+        margin-bottom: 20px;
+        height: 105px;
+        text-decoration: none;
+        transition: .2s}
+      
+      .news_container a:hover{background-color:whitesmoke;transform:scale(1.05)}
 
+      .single-blog-post {
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);}
+      .d-flex {
+        display: -webkit-box!important;
+        display: flex!important;}
+      .single-blog-post.post-style-2 .post-thumbnail {
+        -webkit-box-flex: 0;
+        flex: 0 0 125px;
+        min-width: 125px;
+        min-height: 100px;
+        margin-right: 15px;}
+      .single-blog-post .post-thumbnail img {
+        width: 100%;
+        height: 100%}
+      .single-blog-post.post-style-2 .post-content {
+        padding: 5px 30px 5px 5px;}
+      .single-blog-post.post-style-2 .post-content p {
+        color:black;
+        font-weight:bold;
+        margin-bottom: 10px;
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;}
+      .single-blog-post .post-content .post-meta p {
+        margin-bottom: 0;
+        font-size: 12px;
+        color: #ff0000;
+        line-height: 1.4;}
+      .single-blog-post .post-content .post-meta p span {
+        font-size: 12px;
+        color: #8d8d8d;}
+      tbody tr:hover{-webkit-text-stroke: medium;}
+      
+      @media (max-width: 1475px){
+      .css-keje6w {min-width: calc(100% - 1.5rem);}
+      .single-blog-post.post-style-2 .post-content {
+        padding: 20px 0 5px 5px;}
+      /*.single-blog-post.post-style-2 .post-thumbnail {
+        min-width: 115px;
+        height: auto}*/ }
     """,
         unsafe_allow_html=True,
     )
 with open("trending_quote.json", "r") as fp:
   trending_quote = json.load(fp)
+with open("news_feed.json", "r") as fp:
+  news_feed = json.load(fp)
+with open("tickers_list.json", "r") as fp:
+  tickers_tuple = tuple(json.load(fp).values())
 
 title_message = st.empty()
 
-stocks = (f"SELECT",'AAPL', 'GOOG', 'MSFT', 'GME',"BTC-USD","BAT-USD","OTHER")
+stocks = (f"SELECT","OTHER") + tickers_tuple
+#stocks = (f"SELECT",'AAPL', 'GOOG', 'MSFT', 'GME',"BTC-USD","BAT-USD","OTHER")
 selected_stock = st.sidebar.selectbox("Select your ticker symbol", stocks)
 
 if selected_stock == "OTHER":
-    other_selected_stock = st.sidebar.text_input("Enter the ticker:")
+    other_selected_stock = st.sidebar.text_input("Enter a ticker")
 
 START_DATE = st.sidebar.date_input("Start date",value= pd.to_datetime("2015-01-01"))
 END_DATE = st.sidebar.date_input("End date",value= pd.to_datetime(date.today().strftime("%Y-%m-%d")))
@@ -200,7 +218,7 @@ predicted_var = st.sidebar.selectbox("Predicted Variable", predictable_vars)
 
 n_years = st.sidebar.slider("Years of predictions: ", 1, 5)
 period = n_years * 365
-button_col, status_col = st.sidebar.columns([0.8,1])
+button_col, status_col = st.sidebar.columns([0.5,1])
 apply_button = button_col.button("Apply")
 #status_changes = status_col.empty()
 
@@ -212,7 +230,7 @@ current_input = {"selected_stock": selected_stock,
 
 
 #                                   ______________Loading_data______________
-@st.cache(suppress_st_warning=True)
+#@st.cache(suppress_st_warning=True)
 def load_data(ticker, START_DATE, END_DATE):
     
     data = yf.download(ticker, START_DATE, END_DATE)
@@ -284,10 +302,10 @@ if apply_button:
   if (selected_stock == f"SELECT") or (selected_stock == "OTHER" and len(other_selected_stock.strip()) == 0):
     pass
   else:
-    if selected_stock == "OTHER" and len(other_selected_stock.strip()) != 0:
-      data = load_data(other_selected_stock, START_DATE, END_DATE)
-    else:
+    if selected_stock != "OTHER":
       data = load_data(selected_stock, START_DATE,END_DATE)
+    else:
+      data = load_data(other_selected_stock, START_DATE, END_DATE)
 
     if data is not None:
       st.session_state.selected_stock = selected_stock
@@ -311,7 +329,7 @@ if st.session_state.display:
     if k in current_input.keys():
       if v != current_input[k]:
         #status_changes.text("changes pending!")
-        status_col.markdown('<p class = "change_alert">changes pending</p>',unsafe_allow_html = True)
+        status_col.markdown('<p class = "change_alert">changes pending!</p>',unsafe_allow_html = True)
 
   data = st.session_state.data
   display_plot_raw_data(data, st.session_state.last_ticker)
@@ -341,21 +359,11 @@ else:
 
   home_right.markdown(f"""
     <h1 class="home_col_1_2_header">Latest news</h1>
-    <table>
-        <thead>
-          <tr class="table100-head">
-            <th class="column1">Symbol</th>
-            <th class="column2">Last Price</th>
-            <th class="column3">Open</th>
-            <th class="column4">Volume</th>
-          </tr>
-        </thead>
-        <tbody>
-          {"".join(["<tr><td class='column1'>{}</td><td class='column2'>{}</td><td class='column3'>{}<td class='column4'>{}</td></tr>".format(v["symbol"], v["currentPrice"], v["open"], v["volume"]) for v in trending_quote.values()])}
-        </tbody>
-      </table>
+    <div class="news_container">
+      {"".join(["<a href='{}' class='single-blog-post post-style-2 d-flex'><div class='post-thumbnail'><img src='{}' alt='News banner'></div><div class='post-content'><p>{}</p><div class='post-meta'><p><span>{}</span> on <span>{}</span></p></div></div></a>".format(v["url"],v["banner_image"],v["title"],v["author"],v["time_published"]) for v in news_feed.values()])}
+    </div>
     """, unsafe_allow_html = True)
-  st.info("Awaiting for instructions...<br>Please choose a ticker symbol on the left and apply the changes to get started.")
+
   #st.markdown("<p>Awaiting for instructions...<br>Please choose a ticker symbol on the left and apply the changes to get started.</p>",
   # unsafe_allow_html = True)
 
@@ -377,3 +385,5 @@ else:
 #T5NJMEN0K7I9NZKJ
 #url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=BTC&topics=financial_markets&limit=8&apikey=T5NJMEN0K7I9NZKJ'
 #url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=CRYPTO:BTC&topics=financial_markets&limit=10&apikey=T5NJMEN0K7I9NZKJ"
+#https://colorlib.com/wp/css3-table-templates/
+#https://investexcel.net/all-yahoo-finance-stock-tickers/
